2e66a5c6b8a9806ed5546c28866c18f921f80449
heyqule <hq.jfax@gmail.com>
2023-08-12 14:54:59 -0400

    Changed Zerg graphics code to use Zerg's AnimationDB for future compatilibity.

--- prototypes/NE_Units/New_Spitter_Graphics_Zerg.lua
+++ prototypes/NE_Units/New_Spitter_Graphics_Zerg.lua
@@ -1,4 +1,5 @@
 require ("util")
+local AnimationDB = require('__erm_zerg_hd_assets__/animation_db')
 
 local ENTITYPATH = NE_Common.zentitypath_unit
 local shadow_tint = { r = 0, g = 0, b = 0, a = 192 }
@@ -8,95 +9,25 @@ local shadow_tint = { r = 0, g = 0, b = 0, a = 192 }
   ----------------------------------------------------------------------------------------------------------------------------------------------------------------
   --- Attack
   function zerg_devourer_attackanimation(name, scale, tint1)
-    return
-    {
-      layers = {
-        {
-            filename = ENTITYPATH .. name .. "/" .. name .. "-attack.png",
-            width = 376,
-            height = 401,
-            frame_count = 5,
-            axially_symmetrical = false,
-            direction_count = 16,
-            scale = scale / 2,
-            animation_speed = 0.2,
-            run_mode = 'forward-then-backward'
-        },
-        {
-            filename = ENTITYPATH .. name .. "/" .. name .. "-attack.png",
-            width = 376,
-            height = 401,
-            frame_count = 5,
-            axially_symmetrical = false,
-            direction_count = 16,
-            scale = scale / 2,
-            tint = shadow_tint,
-            animation_speed = 0.2,
-            run_mode = 'forward-then-backward',
-            draw_as_shadow = true,
-            shift = { 4, 0 }
-
-        }
-    }
-    }
+    local animation =  AnimationDB.get_layered_animations('units', 'devourer', 'attack', scale / 2)
+    tint1.a = tint1.a / 2
+    animation = AnimationDB.alter_team_color(animation, tint1)
+    return animation
   end
 
   --- Run
   function zerg_devourer_runanimation(name, scale, tint1)
-    return
-    {
-      layers = {
-        {
-            filename = ENTITYPATH .. name .. "/" .. name .. "-run.png",
-            width = 376,
-            height = 401,
-            frame_count = 5,
-            axially_symmetrical = false,
-            direction_count = 16,
-            scale = scale / 2,
-            animation_speed = 0.6,
-            run_mode = 'forward-then-backward'
-        },
-        {
-            filename = ENTITYPATH .. name .. "/" .. name .. "-run.png",
-            width = 376,
-            height = 401,
-            frame_count = 5,
-            axially_symmetrical = false,
-            direction_count = 16,
-            scale = scale / 2,
-            tint = shadow_tint,
-            shift = { 4, 0 },
-            animation_speed = 0.6,
-            draw_as_shadow = true,
-            run_mode = 'forward-then-backward'
-        }
-    }
-    }
+    local animation =  AnimationDB.get_layered_animations('units', 'devourer', 'run', scale / 2)
+    tint1.a = tint1.a / 2
+    animation = AnimationDB.alter_team_color(animation, tint1)
+    return animation
   end
   
   
 --- DIE
 function zerg_devourer_dieanimation(name, scale, tint1)
-  return
-  {
-    layers=
-    {
-
-      {
-        filename = ENTITYPATH .. name .. "/" .. name .. "-death.png",
-        width = 552,
-        height = 658,
-        frame_count = 8,
-        direction_count = 1,
-        axially_symmetrical = false,
-        scale = scale / 2.2,
-        animation_speed = 0.5
-    }
-
-
-    }
-  }
+  local animation =  AnimationDB.get_single_animation('units', 'devourer', 'corpse', 'main', scale / 2.2)
+  return animation
 end
 
 
@@ -107,91 +38,24 @@ end
   ----------------------------------------------------------------------------------------------------------------------------------------------------------------
   --- Attack
   function zerg_hydralisk_attackanimation(name, scale, tint1)
-    return
-    {
-      layers = {
-        {
-            filename = ENTITYPATH .. name .. "/" .. name .. "-attack.png",
-            width = 258,
-            height = 300,
-            frame_count = 6,
-            axially_symmetrical = false,
-            direction_count = 16,
-            scale = scale,
-            animation_speed = 0.5,
-            shift = {1, 0}
-        },
-        {
-            filename = ENTITYPATH .. name .. "/" .. name .. "-attack.png",
-            width = 258,
-            height = 300,
-            frame_count = 6,
-            axially_symmetrical = false,
-            direction_count = 16,
-            scale = scale,
-            tint = shadow_tint,
-            draw_as_shadow = true,
-            animation_speed = 0.5,
-            shift = {1.2, 0}
-        }
-    }
-    }
+    local animation =  AnimationDB.get_layered_animations('units', 'hydralisk', 'attack', scale)
+    tint1.a = tint1.a / 2
+    animation = AnimationDB.alter_team_color(animation, tint1)
+    return animation
   end
 
   --- Run
   function zerg_hydralisk_runanimation(name, scale, tint1)
-    return
-    {
-      layers = {
-        {
-            filename = ENTITYPATH .. name .. "/" .. name .. "-run.png",
-            width = 257,
-            height = 268,
-            frame_count = 6,
-            axially_symmetrical = false,
-            direction_count = 16,
-            scale = scale,
-            animation_speed = 0.6,
-            shift = {1, 0},
-        },
-        {
-            filename = ENTITYPATH .. name .. "/" .. name .. "-run.png",
-            width = 257,
-            height = 268,
-            frame_count = 6,
-            axially_symmetrical = false,
-            direction_count = 16,
-            scale = scale,
-            tint = shadow_tint,
-            draw_as_shadow = true,
-            animation_speed = 0.6,
-            shift = {1.2, 0}
-        }
-    }
-    }
+    local animation =  AnimationDB.get_layered_animations('units', 'hydralisk', 'run', scale)
+    tint1.a = tint1.a / 2
+    animation = AnimationDB.alter_team_color(animation, tint1)
+    return animation
   end
   
   --- DIE
   function zerg_hydralisk_dieanimation(name, scale, tint1)
-    return
-    {
-      layers=
-      {
-  {
-        
-          filename = ENTITYPATH .. name .. "/" .. name .. "-death.png",
-          width = 549,
-          height = 528,
-          frame_count = 8,
-          direction_count = 1,
-          axially_symmetrical = false,
-          scale = scale,
-          animation_speed = 0.2
-        }
-  
-  
-      }
-    }
+    local animation =  AnimationDB.get_single_animation('units', 'hydralisk', 'corpse', 'main', scale)
+    return animation
   end
 
 
@@ -201,101 +65,22 @@ end
   ----------------------------------------------------------------------------------------------------------------------------------------------------------------
   --- Attack
   function zerg_overlord_attackanimation(name, scale, tint1)
-    return
-    {
-      layers = {
-        {
-          filename = ENTITYPATH .. name .. "/" .. name .. "-run.png",
-          width = 331,
-          height = 364,
-          frame_count = 4,
-          axially_symmetrical = false,
-          direction_count = 16,
-          scale = scale / 2,
-          render_layer = "air-object",
-          run_mode = "forward-then-backward",
-          animation_speed = 0.6,
-      },
-      {
-          filename = ENTITYPATH .. name .. "/" .. name .. "-run.png",
-          width = 331,
-          height = 364,
-          frame_count = 4,
-          axially_symmetrical = false,
-          direction_count = 16,
-          scale = scale / 2,
-          tint = shadow_tint,
-          render_layer = "air-object",
-          animation_speed = 0.6,
-          draw_as_shadow = true,
-          run_mode = "forward-then-backward",
-          shift = { 4, 0 }
-      }
-     }
-   }
-    
+    local animation = AnimationDB.get_layered_animations('units', 'overlord', 'run', scale / 2)
+    return animation
   end
 
   --- Run
   function zerg_overlord_runanimation(name, scale, tint1)
-    return
-    {
-      layers = {
-        {
-          filename = ENTITYPATH .. name .. "/" .. name .. "-run.png",
-          width = 331,
-          height = 364,
-          frame_count = 4,
-          axially_symmetrical = false,
-          direction_count = 16,
-          scale = scale / 2,
-          render_layer = "air-object",
-          run_mode = "forward-then-backward",
-          animation_speed = 0.6
-      },
-      {
-          filename = ENTITYPATH .. name .. "/" .. name .. "-run.png",
-          width = 331,
-          height = 364,
-          frame_count = 4,
-          axially_symmetrical = false,
-          direction_count = 16,
-          scale = scale / 2,
-          render_layer = "air-object",
-          tint = shadow_tint,
-          shift = { 4, 0 },
-          animation_speed = 0.6,
-          run_mode = "forward-then-backward",
-          draw_as_shadow = true
-      }
-    }
-
-
-
-
-    
-    }
+    local animation =  AnimationDB.get_layered_animations('units', 'overlord', 'run', scale / 2)
+    tint1.a = tint1.a / 2
+    animation = AnimationDB.alter_team_color(animation, tint1)
+    return animation
   end
   
   --- DIE
   function zerg_overlord_dieanimation(name, scale, tint1)
-    return
-    {
-      layers=
-      {
-        {
-        filename = ENTITYPATH .. name .. "/" .. name .. "-death.png",
-        width = 552,
-        height = 658,
-        frame_count = 8,
-        direction_count = 1,
-        axially_symmetrical = false,
-        scale = scale / 2,
-        render_layer = "air-object",
-        animation_speed = 0.5
-      }
-      }
-    }
+    local animation =  AnimationDB.get_single_animation('units', 'overlord', 'corpse', 'main', scale / 2)
+    return animation
   end
 
 
@@ -304,97 +89,24 @@ end
   ----------------------------------------------------------------------------------------------------------------------------------------------------------------
   --- Attack
   function zerg_queen_attackanimation(name, scale, tint1)
-    return
-    {
-      layers = {
-        {
-          filename = ENTITYPATH .. name .. "/" .. name .. "-attack.png",
-          width = 538,
-          height = 512,
-          frame_count = 5,
-          axially_symmetrical = false,
-          direction_count = 16,
-          scale = scale / 2,
-          render_layer = "air-object",
-          animation_speed = 0.6
-      },
-      {
-          filename = ENTITYPATH .. name .. "/" .. name .. "-attack.png",
-          width = 538,
-          height = 512,
-          frame_count = 5,
-          axially_symmetrical = false,
-          direction_count = 16,
-          shift = { 4, 0 },
-          scale = scale / 2,
-          render_layer = "air-object",
-          draw_as_shadow = true,
-          tint = shadow_tint,
-          animation_speed = 0.6,
-          shift = {4, 0}
-      }
-     }
-   }
-    
+      local animation =  AnimationDB.get_layered_animations('units', 'queen', 'attack', scale / 2)
+      tint1.a = tint1.a / 2
+      animation = AnimationDB.alter_team_color(animation, tint1)
+      return animation
   end
 
   --- Run
   function zerg_queen_runanimation(name, scale, tint1, tint2)
-    return
-    {
-      layers = {
-        {
-          filename = ENTITYPATH .. name .. "/" .. name .. "-run.png",
-          width = 538,
-          height = 512,
-          frame_count = 6,
-          axially_symmetrical = false,
-          direction_count = 16,
-          scale = scale / 2,
-          render_layer = "air-object",
-          animation_speed = 1,
-      },
-      {
-          filename = ENTITYPATH .. name .. "/" .. name .. "-run.png",
-          width = 538,
-          height = 512,
-          frame_count = 6,
-          axially_symmetrical = false,
-          direction_count = 16,
-          shift = { 4, 0 },
-          scale = scale / 2,
-          render_layer = "air-object",
-          tint = shadow_tint,
-          draw_as_shadow = true,
-          animation_speed = 1,
-          shift = {4, 0}
-      }
-    }
-
-
-
-
-    
-    }
+    local animation =  AnimationDB.get_layered_animations('units', 'queen', 'run', scale / 2)
+    tint1.a = tint1.a / 2
+    animation = AnimationDB.alter_team_color(animation, tint1)
+    return animation
   end
   
   --- DIE
   function zerg_queen_dieanimation(name, scale, tint1)
-    return
-    {
-      layers=
-      {
-        {
-          filename = ENTITYPATH .. name .. "/" .. name .. "-death.png",
-          width = 552,
-          height = 650,
-          frame_count = 8,
-          direction_count = 1,
-          axially_symmetrical = false,
-          scale = scale / 2,
-          render_layer = "air-object",
-          animation_speed = 0.5
-      }
-      }
-    }
+    local animation =  AnimationDB.get_single_animation('units', 'queen', 'corpse', 'main', scale / 2)
+    tint1.a = tint1.a / 2
+    animation = AnimationDB.alter_team_color(animation, tint1)
+    return animation
   end
